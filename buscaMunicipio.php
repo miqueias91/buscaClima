f0d27b708336ef94e9c8c5d6b92af4df67af85ff
Miqueias Matias Caetano <miqueias.caetano@doctum.edu.br>
2019-03-28 17:20:20 -0300

    corrigindo erro ao buscar municipios

--- buscaMunicipio.php
+++ buscaMunicipio.php
@@ -2,12 +2,43 @@
 	$uf = $_POST['uf'];
 
 	//FUNCAO CURL PARA CONEXA REMOTA, USADA PARA CONECTAR AO WEBSERVICE
-	function CURL($url){
-		$ch   	= 	curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-		return $result;
+	function ConnURLPost($url,$options,$timeout = 20){
+
+		//Montando as opções do CURL
+		$contentLength = "Content-length: ".strlen($options);
+		$methodOptions = Array(
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $options,
+		);
+	
+		$options = Array(
+			CURLOPT_HTTPHEADER => Array(
+					"Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+					$contentLength
+			),
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HEADER => false,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_CONNECTTIMEOUT => $timeout,
+			CURLOPT_TIMEOUT => $timeout
+		);
+		$options = ($options + $methodOptions);
+	
+		//INICIA CONEXAO
+		$curl = curl_init();
+		curl_setopt_array($curl, $options);
+		$resp  = curl_exec($curl);
+		$info  = curl_getinfo($curl);
+		$error = curl_errno($curl);
+		$errorMessage = curl_error($curl);
+		if($error){
+			return false ;
+		}
+		//FECHA CONEXAO
+		curl_close($curl);
+	
+		return $resp;
 	}
 
 	//BUSCA OS ESTADOS DO BRASIL
@@ -15,7 +46,7 @@
 
 
 
-	$result0 = CURL($url0);
+	$result0 = ConnURLPost($url0);
 	$resultadoMunicipios = json_decode($result0);
 
 	if ($resultadoMunicipios) {
